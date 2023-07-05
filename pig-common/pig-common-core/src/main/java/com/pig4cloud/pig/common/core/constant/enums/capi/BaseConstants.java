1f6184cf5dfd5cd2b5754afc47739ccd8ee5c515
gavin.Q <906827215@qq.com>
2023-07-05 10:01:35 +0800

    1、添加基本常数

---
+++ pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/constant/enums/capi/BaseConstants.java
@@ -0,0 +1,12 @@
+package com.pig4cloud.pig.common.core.constant.enums.capi;
+
+public interface BaseConstants {
+
+	/**
+	 * 机器人需要代理 1 需要 0不需要
+	 */
+	Integer NEED_PROXY = 1;
+
+	Integer NOT_NEED_PROXY = 0;
+
+}
