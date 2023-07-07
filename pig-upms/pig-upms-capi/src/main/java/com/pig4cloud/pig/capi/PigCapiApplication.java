a90038a4e59c8d6d4a7c7cb4e4a44ae36b72f83d
gavin.Q <906827215@qq.com>
2023-07-07 09:55:48 +0800

    启动器瘦身

--- pig-upms/pig-upms-capi/src/main/java/com/pig4cloud/pig/capi/PigCapiApplication.java
+++ pig-upms/pig-upms-capi/src/main/java/com/pig4cloud/pig/capi/PigCapiApplication.java
@@ -1,14 +1,8 @@
 package com.pig4cloud.pig.capi;
 
-import com.pig4cloud.pig.common.security.annotation.EnablePigResourceServer;
-import com.pig4cloud.pig.common.swagger.annotation.EnablePigDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnablePigDoc
-@EnablePigResourceServer
-@EnableDiscoveryClient
 @SpringBootApplication
 public class PigCapiApplication{
 
