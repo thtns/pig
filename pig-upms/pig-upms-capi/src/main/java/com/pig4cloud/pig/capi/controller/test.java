828651f4eac256acb55ed551d5a2c844fe5af4df
gavin.Q <906827215@qq.com>
2023-07-20 14:24:21 +0800

    测试接口test

--- pig-upms/pig-upms-capi/src/main/java/com/pig4cloud/pig/capi/controller/test.java
+++ pig-upms/pig-upms-capi/src/main/java/com/pig4cloud/pig/capi/controller/test.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 public class test {
@@ -17,14 +19,16 @@ public class test {
 		return "你看得到我么？";
 	}
 
+	@PostMapping(value = {"/test2"})
+	public String test2(HttpServletRequest request, @RequestBody String requestBody) {
+		log.info("callback 再回调给商家的数据是：{}", JSON.toJSONString(requestBody));
+		return "你看得到我么？ 数据是：\n {}" + JSON.toJSONString(requestBody);
+	}
+
 	@GetMapping(value = {"/test3"})
 	public R test3(HttpServletRequest request) {
 		return R.ok();
 	}
 
-	@PostMapping(value = {"/test2"})
-	public String test2(HttpServletRequest request, @RequestBody String requestBody) {
-		log.info("callback 再回调给商家的数据是：{}", JSON.toJSONString(requestBody));
-		return "你看得到我么2？";
-	}
+
 }
