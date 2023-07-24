4eef75c02bfb02ebb4f3f41ce79ddd08ef1e28bf
gavin.Q <906827215@qq.com>
2023-07-24 14:40:22 +0800

    1、添加一个扫描路劲,不然mq的bean找不到

--- pig-upms/pig-upms-capi/src/main/java/com/pig4cloud/pig/capi/PigCapiApplication.java
+++ pig-upms/pig-upms-capi/src/main/java/com/pig4cloud/pig/capi/PigCapiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.pig4cloud.pig.capi", "com.pig4cloud.pig.common.core"})
 public class PigCapiApplication{
 
 	public static void main(String[] args) {
